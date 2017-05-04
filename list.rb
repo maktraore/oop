e8733546561ffa5b0bad555522a33c371a6d31e3
Mamadou Traore <maktraore2000@gmail.com>
2017-05-04 00:21:09 -0400

    added more code to list and task

--- list.rb
+++ list.rb
@@ -1,8 +1,11 @@
 class List
-  attr_reader :tasks
-  
+
   def initialize
     @tasks = []
+  end
+
+  def tasks
+    return @tasks
   end
 
   def add_task(task)
@@ -17,5 +20,20 @@ class List
       end
     end
     return task_names
+  end
+  def sort_by_priority
+    @tasks.sort_by {|a| a.priority}
+  end
+#6
+  def number_of_incomplete_tasks
+    incomplete_task_names.length
+  end
+#7
+  def delete_complete_tasks
+    i=0 
+    @tasks.each do |task|
+      @tasks.delete(task) if task.complete
+     end
+    @tasks
   end
 end
--- task.rb
+++ task.rb
@@ -1,22 +1,63 @@
 class Task
-  attr_reader :name
-  attr_accessor :complete
-
+  
   def initialize(name)
     @name = name
     @complete = false
+#8a
+    @priority = 1
+  end
+#8a
+  def set_priority=(priority)
+    @priority = priority
+  end
+#8.
+  def sort_by_priority
+    @priority
+  end
+#5
+  def toggle_complete!
+    @complete = !@complete
+  end
+  def name
+    return @name
+  end
+  def delete_complete_tasks
+    return @delete_complete_tasks
+  end
+  def complete
+    return @complete
   end
 
   def complete!
     @complete = true
   end
-
-  def toggle_complete! 
-    @complete = !@complete
-    # if @complete
-    #   @complete = false
-    # else
-    #   @complete = true
-    # end
-  end
 end
+# Create list
+list = List.new
+
+# Create tasks and add them to the list
+list.add_task(Task.new("Feed the cat"))
+list.add_task(Task.new("Take out trash"))
+list.add_task(Task.new("Mow the lawn"))
+
+# Print out the second task in the list
+puts "Second task:"
+puts list.tasks[1].name
+puts "---------"
+
+# Get an array containing the names of all incomplete tasks from the list
+puts "Incomplete Tasks:"
+puts list.incomplete_task_names
+puts "--------"
+puts "number of incomplete tasks: #{list.number_of_incomplete_tasks}"
+puts "--------"
+# Mark the first task from the list as complete
+list.tasks[0].complete!
+list.tasks[2].toggle_complete!
+list.tasks[0].set_priority=2
+p list.tasks
+p list.tasks.delete_complete_tasks
+p list.tasks.sort_by_priority
+# Print out the incomplete tasks again
+puts "Incomplete Tasks:"
+puts list.incomplete_task_names
